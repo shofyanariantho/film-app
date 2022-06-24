e4202b8185a5fd5d3a43260d08cf8e01c1a78eee
shofyanariantho <shofyanariantho@gmail.com>
2022-06-24 10:43:22 +0700

    Update: Off verify login

--- backend/src/routes/actorRouter.js
+++ backend/src/routes/actorRouter.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const actorController = require('../controllers/actorController');
-const { verifyLogin } = require('../middleware/verifyLogin'); // Middleware for Auth
-const { verifyToken } = require('../middleware/verifyToken'); // Middleware for Auth
+const express = require("express");
+const actorController = require("../controllers/actorController");
+const { verifyLogin } = require("../middleware/verifyLogin"); // Middleware for Auth
+const { verifyToken } = require("../middleware/verifyToken"); // Middleware for Auth
 const router = express.Router();
 
-router.post('/create', actorController.create); // Create
-router.get('/', actorController.index); // Index
-router.get('/:id', actorController.show); // Show
-router.put('/:id', verifyLogin, actorController.update); // Update
-router.delete('/:id', verifyLogin, actorController.destroy); // Delete
-router.post('/image/:id', verifyLogin, actorController.upload); // Upload Image
+router.post("/create", actorController.create); // Create
+router.get("/", actorController.index); // Index
+router.get("/:id", actorController.show); // Show
+router.put("/:id", actorController.update); // Update
+router.delete("/:id", actorController.destroy); // Delete
+router.post("/image/:id", actorController.upload); // Upload Image
 
 module.exports = router;
