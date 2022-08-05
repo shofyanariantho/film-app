d63926ad2d4869371d9cc07d0035a1b5c37647a1
Surya Saputra <95985937+Suryasap007@users.noreply.github.com>
2022-08-05 20:42:17 +0700

    push 8 agt 2022

--- backend/src/db/knexfile.js
+++ backend/src/db/knexfile.js
@@ -10,9 +10,9 @@ module.exports = {
     client: "mysql2",
     connection: {
       host: "127.0.0.1",
-      port: 3307,
+      port: 3306,
       user: "root",
-      password: "gwsudahbahagia",
+      password: "",
       database: "film_app",
     },
     migrations: {
