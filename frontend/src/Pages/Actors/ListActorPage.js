4032a31ad9447279cf7760a0c7cf8366973a6f1c
shofyanariantho <shofyanariantho@gmail.com>
2022-08-13 00:50:27 +0700

    Update

--- frontend/src/Pages/Actors/ListActorPage.js
+++ frontend/src/Pages/Actors/ListActorPage.js
@@ -1,42 +1,47 @@
-import React from 'react'
-import { ActorList, Footer, NavbarComponent, PageTitle } from '../../components'
-import bgformdirector from "../../assets/images/bg/bgformdirector.jpg"
-import { Card } from 'react-bootstrap'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
+import React from "react";
+import {
+  ActorList,
+  Footer,
+  NavbarComponent,
+  PageTitle,
+} from "../../components";
+import bgformdirector from "../../assets/images/bg/bg.jpg";
+import { Card } from "react-bootstrap";
+import { GrClose } from "react-icons/gr";
 
 const ListActorPage = () => {
-    return (
-        <div style={{ backgroundImage: `url(${bgformdirector})` }}>
-            <NavbarComponent />
-            <div className="container h-100">
-                <div className="row h-100 my-5">
-                    <div className="col-12 mx-auto">
-                        <Card style={{ height: "100vh" }}>
-                            <Card.Header className="align-items-center">
-                                <div className="d-flex justify-content-between">
-                                    <Card.Title>
-                                        <b>Reviewed Actors</b>
-                                    </Card.Title>
+  return (
+    <div style={{ backgroundImage: `url(${bgformdirector})` }}>
+      <NavbarComponent />
+      <div className="container h-100 py-5">
+        <div className="row h-100">
+          <div className="col-12 mx-auto">
+            <Card style={{ height: "100vh" }}>
+              <Card.Header className="align-items-center">
+                <div className="d-flex justify-content-between">
+                  <Card.Title>
+                    <b>Reviewed Actors</b>
+                  </Card.Title>
 
-                                    <a href="/" className="text-dark">
-                                        <AiOutlineCloseCircle className="fs-4" />
-                                    </a>
-                                </div>
-                            </Card.Header>
-
-                            {/* Content */}
-                            <div className="card-body p-0">
-                                <div className="table-responsive">
-                                    <ActorList />
-                                </div>
-                            </div>
-                        </Card>
-                    </div>
+                  <a href="/" className="text-dark">
+                    <GrClose className="fs-5" />
+                  </a>
                 </div>
-            </div>
-            <Footer />
-        </div>
-    )
-}
+              </Card.Header>
 
-export default ListActorPage
+              {/* Content */}
+              <div className="card-body p-0">
+                <div className="table-responsive">
+                  <ActorList />
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ListActorPage;
--- frontend/src/components/Actors/ActorList.js
+++ frontend/src/components/Actors/ActorList.js
@@ -1,139 +1,146 @@
-import React, { useContext, useEffect, useState } from 'react'
-import axios from 'axios';
-import { Button, Form, Table } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Button, Form, Table } from "react-bootstrap";
 import { UserContext } from "../../utils/UserContext";
-import { AiFillDelete, AiFillPlusCircle, AiOutlineEdit } from 'react-icons/ai';
+import {
+  AiOutlineDelete,
+  AiFillPlusCircle,
+  AiOutlineEdit,
+  AiOutlineUpload,
+} from "react-icons/ai";
 
 const ActorList = () => {
-    const [actors, setActors] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const { user } = useContext(UserContext);
+  const [actors, setActors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        getActors();
-        deleteActor();
-    }, []);
+  useEffect(() => {
+    getActors();
+    deleteActor();
+  }, []);
 
-    const getActors = async () => {
-        const { data: res } = await axios.get("http://localhost:8000/actor", {
-            withCredentials: true
-        })
-            .then(async (response) => {
-                const actors = response.data;
-                setActors(actors);
-            });
-    };
-    const deleteActor = async (id) => {
-        try {
-            await axios.delete(`http://localhost:8000/actor/${id}`,
-                { withCredentials: true }
-            );
-            getActors();
-        } catch (error) {
-        }
-    };
-    return (
-        <div className="p-3">
-            {!user ? (
-                <Form.Control
-                    className="py-1 mb-3"
-                    type="string"
-                    placeholder="Search..."
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            ) : (
-                <div className="row mb-3 align-items-center justify-content-between">
-                    <div className="col-6 align-items-center">
-                        <a href="/createactor" className="btn btn-warning">
-                            <AiFillPlusCircle className="fs-4 pb-1" />
-                            <span>
-                                {" "}
-                                <b>Add New</b>
-                            </span>
-                        </a>
-                    </div>
+  const getActors = async () => {
+    const { data: res } = await axios
+      .get("http://localhost:8000/actor", {
+        withCredentials: true,
+      })
+      .then(async (response) => {
+        const actors = response.data;
+        setActors(actors);
+      });
+  };
+  const deleteActor = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8000/actor/${id}`, {
+        withCredentials: true,
+      });
+      getActors();
+    } catch (error) {}
+  };
+  return (
+    <div className="p-3">
+      {!user ? (
+        <Form.Control
+          className="py-1 mb-3"
+          type="string"
+          placeholder="Search..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      ) : (
+        <div className="row mb-3 align-items-center justify-content-between">
+          <div className="col-6 align-items-center">
+            <a href="/createactor" className="btn btn-warning">
+              <AiFillPlusCircle className="fs-4 pb-1" />
+              <span>
+                {" "}
+                <b>Add New</b>
+              </span>
+            </a>
+          </div>
 
-                    <div className="col-6">
-                        <Form.Control
-                            type="string"
-                            placeholder="Search..."
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                    </div>
-                </div>
-            )}
-
-            <Table hover>
-                <thead>
-                    {!user ? (
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                        </tr>
-                    ) : (
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Actions</th>
-                        </tr>
-                    )}
-                </thead>
-                <tbody>
-                    {actors
-                        .filter((actor) => {
-                            if (searchTerm === "") {
-                                return actor;
-                            } else if (
-                                actor.actorName
-                                    .toLowerCase()
-                                    .includes(searchTerm.toLocaleLowerCase())
-                            ) {
-                                return actor;
-                            }
-                        })
-                        .map((actor, index) => {
-                            if (!user) {
-                                return (
-                                    <tr key={actor.id}>
-                                        <td>{index + 1}</td>
-                                        <td>{actor.actorName}</td>
-                                    </tr>
-                                );
-                            }
-                            return (
-                                <tr key={actor.id}>
-                                    <td>{index + 1}</td>
-                                    <td>{actor.actorName}</td>
-                                    <td className="col-md-2">
-                                        <Button
-                                            href={`createfilmimage/${actor.id}`}
-                                            variant="primary"
-                                            size="sm"
-                                        >
-                                            Upload Image
-                                        </Button>{" "}
-                                        <Button
-                                            href={`updatefilm/${actor.id}`}
-                                            variant="success"
-                                            size="sm"
-                                        >
-                                            <AiOutlineEdit className="fs-5" />
-                                        </Button>{" "}
-                                        <Button
-                                            onClick={() => deleteActor(actor.id)}
-                                            variant="danger"
-                                            size="sm"
-                                        >
-                                            <AiFillDelete className="fs-5" />
-                                        </Button>{" "}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-            </Table>
+          <div className="col-6">
+            <Form.Control
+              type="string"
+              placeholder="Search..."
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
         </div>
-    )
-}
+      )}
 
-export default ActorList
+      <Table hover>
+        <thead>
+          {!user ? (
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+            </tr>
+          ) : (
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th></th>
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          {actors
+            .filter((actor) => {
+              if (searchTerm === "") {
+                return actor;
+              } else if (
+                actor.actorName
+                  .toLowerCase()
+                  .includes(searchTerm.toLocaleLowerCase())
+              ) {
+                return actor;
+              }
+            })
+            .map((actor, index) => {
+              if (!user) {
+                return (
+                  <tr key={actor.id}>
+                    <td>{index + 1}</td>
+                    <td>{actor.actorName}</td>
+                  </tr>
+                );
+              }
+              return (
+                <tr key={actor.id} className="align-middle p-2">
+                  <td>{index + 1}</td>
+                  <td>{actor.actorName}</td>
+                  <td className="col-md-3">
+                    <Button
+                      href={`createfilmimage/${actor.id}`}
+                      variant="default"
+                      size="sm"
+                    >
+                      <AiOutlineUpload className="fs-5 me-2" />
+                      Image
+                    </Button>
+                    <Button
+                      href={`updatefilm/${actor.id}`}
+                      variant="default"
+                      size="sm"
+                    >
+                      <AiOutlineEdit className="fs-5 me-2" />
+                      Edit
+                    </Button>
+                    <Button
+                      onClick={() => deleteActor(actor.id)}
+                      variant=""
+                      size="sm"
+                    >
+                      <AiOutlineDelete className="fs-5 me-2" />
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default ActorList;
